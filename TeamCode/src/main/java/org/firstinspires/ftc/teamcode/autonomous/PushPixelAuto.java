b08a29f7ea54734b476f7611ff795300688b100e
Ben <134473086+BenVong@users.noreply.github.com>
2023-12-10 22:43:20 -0700

    Update PushPixelAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PushPixelAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PushPixelAuto.java
@@ -18,7 +18,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@Autonomous(name = "Auto: Push Pixel", group = "Autonomous")
+@Autonomous(name = "Auto: Push Pixel TF", group = "Autonomous")
 public class PushPixelAuto extends LinearOpMode {
     //Used to gain position from x and y from telemetry of the pixel
     double XPosition_Pixel = 0;
