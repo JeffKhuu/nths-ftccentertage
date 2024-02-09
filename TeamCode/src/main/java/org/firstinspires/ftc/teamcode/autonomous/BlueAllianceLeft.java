76f9f3852ec05d4b2423515715f21244cbecc25a
Dark-Arashi <Emailtosehaj@gmail.com>
2024-02-08 21:43:35 -0700

    Blue alliance done magic numbers need to be fine tuned just the reverse of this will be red alliance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAllianceLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAllianceLeft.java
@@ -33,16 +33,21 @@ public class BlueAllianceLeft extends LinearOpMode {
             actions.add(new RobotPath(DriveTrain.BACKWARD, 1.0));
             actions.add(new RobotPath(DriveTrain.RIGHT, 0.6));
             actions.add(new RobotPath(DriveTrain.SPIN_CCW, 1.3));
-            actions.add(new RobotPath(DriveTrain.FORWARD, 1.4));
-            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 1.0, 0));
-            actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, 1.0, 1.0, 0));
+            actions.add(new RobotPath(DriveTrain.BACKWARD, 1.4));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
 
-            actions.add(new RobotPath(DriveTrain.BACKWARD, 2.0));
+            actions.add(new RobotPath(DriveTrain.FORWARD, 0.8));
             actions.add(new RobotPath(DriveTrain.LEFT, 3.2));
             actions.add(new RobotPath(DriveTrain.SPIN_CCW,1.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
 
             actionExecutor.runPaths(actions);
-
+            actions.clear();
         } else {
             actions.add(new RobotPath(DriveTrain.SPIN_CCW, 0.3));
             actions.add(new RobotPath(DriveTrain.LEFT, 0.5));
@@ -52,27 +57,47 @@ public class BlueAllianceLeft extends LinearOpMode {
                 actions.add(new RobotPath(DriveTrain.BACKWARD, 1.0));
                 actions.add(new RobotPath(DriveTrain.RIGHT, 0.6));
                 actions.add(new RobotPath(DriveTrain.SPIN_CW, 1.9));
-                actions.add(new RobotPath(DriveTrain.FORWARD, 0.4));
+                actions.add(new RobotPath(DriveTrain.BACKWARD, 0.4));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
 
-                actions.add(new RobotPath(DriveTrain.BACKWARD, 3.0));
+                actions.add(new RobotPath(DriveTrain.FORWARD, 1.3));
                 actions.add(new RobotPath(DriveTrain.RIGHT, 2.0));
-                actions.add(new RobotPath(DriveTrain.FORWARD, 3.2));
+                actions.add(new RobotPath(DriveTrain.BACKWARD, 3.2));
                 actions.add(new RobotPath(DriveTrain.LEFT,2.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
 
                 actionExecutor.runPaths(actions);
+                actions.clear();
 
             } else {
                 actions.add(new RobotPath(DriveTrain.SPIN_CCW, 0.9));
                 actions.add(new RobotPath(DriveTrain.FORWARD, 2.0));
 
                 if (robotHardware.isTouchSensorTouched()) {
-                    actions.add(new RobotPath(DriveTrain.BACKWARD, 1.0));
+                    actions.add(new RobotPath(DriveTrain.BACKWARD, 1.3));
                     actions.add(new RobotPath(DriveTrain.RIGHT, 0.6));
-                    actions.add(new RobotPath(DriveTrain.FORWARD, 1.0));
-                    actions.add(new RobotPath(DriveTrain.BACKWARD, 3.0));
-                    actions.add(new RobotPath(DriveTrain.RIGHT, 2.0));
+                    actions.add(new RobotPath(DriveTrain.SPIN_CW, 1.9));
+                    actions.add(new RobotPath(DriveTrain.BACKWARD, 0.4));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
+                    actions.add(new RobotPath(DriveTrain.LEFT, 0.5));
+                    actions.add(new RobotPath(DriveTrain.FORWARD, 3.0));
+
                     actions.add(new RobotPath(DriveTrain.FORWARD, 3.2));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
+
+
                     actionExecutor.runPaths(actions);
+                    actions.clear();
                 }
             }
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAllianceRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAllianceRight.java
@@ -4,6 +4,112 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.systems.ActionExecutor;
+import org.firstinspires.ftc.teamcode.systems.DriveTrain;
+import org.firstinspires.ftc.teamcode.systems.RobotHardware;
+import org.firstinspires.ftc.teamcode.systems.RobotPath;
+
+import java.util.ArrayList;
+
+@Autonomous(name="Auto: Blue Alliance Left", group="Autonomous")
+public class BlueAllianceRight extends LinearOpMode {
+
+    private final ElapsedTime runtime = new ElapsedTime();
+    private final ActionExecutor actionExecutor = new ActionExecutor(this, runtime);
+    private final RobotHardware robotHardware = new RobotHardware(this);
+    private final ArrayList<RobotPath> actions = new ArrayList<>();
+
+    @Override
+    public void runOpMode() {
+        actionExecutor.init();
+        robotHardware.init();
+
+        waitForStart();
+
+        actions.add(new RobotPath(DriveTrain.FORWARD, 1.5));
+        actionExecutor.runPaths(actions);
+
+        if (robotHardware.isTouchSensorTouched()) {
+            actions.add(new RobotPath(DriveTrain.BACKWARD, 1.0));
+            actions.add(new RobotPath(DriveTrain.LEFT, 0.6));
+            actions.add(new RobotPath(DriveTrain.SPIN_CCW, 1.3));
+            actions.add(new RobotPath(DriveTrain.BACKWARD, 1.4));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
+
+            actions.add(new RobotPath(DriveTrain.FORWARD, 0.8));
+            actions.add(new RobotPath(DriveTrain.RIGHT, 3.2));
+            actions.add(new RobotPath(DriveTrain.SPIN_CCW,0.8));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+            actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
+
+            actionExecutor.runPaths(actions);
+            actions.clear();
+        } else {
+            actions.add(new RobotPath(DriveTrain.SPIN_CCW, 0.3));
+            actions.add(new RobotPath(DriveTrain.LEFT, 0.5));
+            actions.add(new RobotPath(DriveTrain.FORWARD, 1.0));
+
+            if (robotHardware.isTouchSensorTouched()) {
+                actions.add(new RobotPath(DriveTrain.BACKWARD, 1.0));
+                actions.add(new RobotPath(DriveTrain.RIGHT, 0.6));
+                actions.add(new RobotPath(DriveTrain.SPIN_CW, 1.9));
+                actions.add(new RobotPath(DriveTrain.BACKWARD, 0.4));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
+
+                actions.add(new RobotPath(DriveTrain.FORWARD, 0.8));
+                actions.add(new RobotPath(DriveTrain.RIGHT, 2.0));
+                actions.add(new RobotPath(DriveTrain.BACKWARD, 1.2));
+                actions.add(new RobotPath(DriveTrain.LEFT,2.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+                actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
+
+                actionExecutor.runPaths(actions);
+                actions.clear();
+
+            } else {
+                actions.add(new RobotPath(DriveTrain.SPIN_CCW, 0.9));
+                actions.add(new RobotPath(DriveTrain.FORWARD, 2.0));
+
+                if (robotHardware.isTouchSensorTouched()) {
+                    actions.add(new RobotPath(DriveTrain.BACKWARD, 1.3));
+                    actions.add(new RobotPath(DriveTrain.RIGHT, 0.6));
+                    actions.add(new RobotPath(DriveTrain.SPIN_CW, 1.9));
+                    actions.add(new RobotPath(DriveTrain.BACKWARD, 0.4));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 3.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 1.5, 1.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 5.0));
+                    actions.add(new RobotPath(DriveTrain.LEFT, 0.5));
+                    actions.add(new RobotPath(DriveTrain.FORWARD, 3.0));
+
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.ARM_MOTOR,1.0,5.0));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.WRIST_SERVO, 1.0, 2.0 ));
+                    actions.add(new RobotPath(RobotPath.UtilizedHardware.INTAKE_SERVO, -1.0, 5.0, 1.0));
+
+
+
+                    actionExecutor.runPaths(actions);
+                    actions.clear();
+                }
+            }
+        }
+    }
+}
+/**
+
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.systems.ActionExecutor;
 import org.firstinspires.ftc.teamcode.systems.AutoOpMode;
@@ -104,3 +210,4 @@ public class BlueAllianceRight extends LinearOpMode implements AutoOpMode {
 
     }
 }
+**/
